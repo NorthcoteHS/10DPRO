e9703d3758491ee3d1796efd8fe0c007bdc7c32e
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-05-10 10:06:58 +1000

    Add comment about function variables

--- assessments/cat2_codeAndResearch/functionDemo.py
+++ assessments/cat2_codeAndResearch/functionDemo.py
@@ -3,6 +3,8 @@ Prog:   functionDemo
 Name:   Mr. Koopmans
 Date:   10/05/18
 Desc:   Demo of how to use a function from *within* a for loop.
+        Note that the function ("action") only deals with its own internal variables,
+        "msg" and "response", which do not exist anywhere outside of the function.
 """
 
 # Define a function that will display a message and return the response.
